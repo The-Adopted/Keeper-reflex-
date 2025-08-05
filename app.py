49ba9b40eef8e6400a77e5024deba5db78043941
Lucilegrae <pnovontony@yahoo.com>
2025-08-05 07:04:07 +0200

    Fix Flask port for Render deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import json
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,4 @@ def capsule_timeline():
         }), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
